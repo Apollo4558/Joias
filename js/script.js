2dbf672445062330b4ddc97924f9a5c7b008c68b
rianfernandesno <zrpapz12@gmail.com>
2025-05-06 16:44:39 -0300

    gallery

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ nameElement.style.webkitBackgroundClip = "text"; // Aplica degradê ao texto
 nameElement.style.webkitTextFillColor = "transparent";
 nameElement.style.fontWeight = "bold";
 nameElement.style.fontSize = 36 + 'px';
+
+function filtrarCategoria(categoria) {
+    const itens = document.querySelectorAll('.item');
+  
+    itens.forEach(item => {
+      if (categoria === 'todas' || item.classList.contains(categoria)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+  
+  // Exibir todos os itens ao carregar a página
+  window.onload = () => filtrarCategoria('todas');
