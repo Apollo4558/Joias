e02ae330caf568bc9078335dea1464ca3c34ec81
rianfernandesno <zrpapz12@gmail.com>
2025-09-06 08:28:35 -0300

    header

---
+++ frontend-react/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../img/Logo.jpg'; // Importa a imagem
+import './Header.css';
+
+const Header = () => {
+  const userName = localStorage.getItem('userName') || 'visitante';
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img src={Logo} alt="Logo" className="header-logo" /> 
+        <div className="header-welcome">
+          <h1>Bem vindo,</h1>
+          <span className="user-name">{userName}</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
