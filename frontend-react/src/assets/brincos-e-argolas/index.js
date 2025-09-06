accf403055b7bb4d4c22faab38fd53b179536367
rianfernandesno <zrpapz12@gmail.com>
2025-09-06 12:01:58 -0300

    colares argolas brincos

--- frontend-react/src/assets/brincos-e-argolas/index.js
+++ frontend-react/src/assets/brincos-e-argolas/index.js
@@ -23,10 +23,32 @@ import brinco20 from './brincos-20.jpeg';
 import brinco21 from './brincos-21.jpeg';
 import brinco22 from './brincos-22.jpeg';
 import brinco23 from './brincos-23.jpeg';
+import brinco24 from './brincos-24.jpeg';
+import brinco25 from './brincos-25.jpeg';
+import brinco26 from './brincos-26.jpeg';
+import brinco27 from './brincos-27.jpeg';
+import brinco28 from './brincos-28.jpeg';
+import brinco29 from './brincos-29.jpeg';
+import brinco30 from './brincos-30.jpeg';
+import brinco31 from './brincos-31.jpeg';
+import brinco32 from './brincos-32.jpeg';
+import brinco33 from './brincos-33.jpeg';
+import brinco34 from './brincos-34.jpeg';
+import brinco35 from './brincos-35.jpeg';
+import brinco36 from './brincos-36.jpeg';
+import brinco37 from './brincos-37.jpeg';
+import brinco38 from './brincos-38.jpeg';
+import brinco39 from './brincos-39.jpeg';
+import brinco40 from './brincos-40.jpeg';
+import brinco41 from './brincos-41.jpeg';
+
 
 export const BRINCOS = [
   brinco1, brinco2, brinco3, brinco4, brinco5, brinco6,
   brinco7, brinco8, brinco9, brinco10, brinco11, brinco12,
   brinco13, brinco14, brinco15, brinco16, brinco17, brinco18,
   brinco19, brinco20, brinco21, brinco22, brinco23,
+  brinco24, brinco25, brinco26, brinco27, brinco28, brinco29,
+  brinco30, brinco31, brinco32, brinco33, brinco34, brinco35,
+  brinco36, brinco37, brinco38, brinco39, brinco40, brinco41,
 ];
--- frontend-react/src/assets/colares/index.js
+++ frontend-react/src/assets/colares/index.js
@@ -4,8 +4,30 @@ import colar3 from './colares-3.jpeg';
 import colar4 from './colares-4.jpeg';
 import colar5 from './colares-5.jpeg';
 import colar6 from './colares-6.jpeg';
+import colar7 from './colares-7.jpeg';
+import colar8 from './colares-8.jpeg';
+import colar9 from './colares-9.jpeg';
+import colar10 from './colares-10.jpeg';
+import colar11 from './colares-11.jpeg';
+import colar12 from './colares-12.jpeg';
+import colar13 from './colares-13.jpeg';
+import colar14 from './colares-14.jpeg';
+import colar15 from './colares-15.jpeg';
+import colar16 from './colares-16.jpeg';
+import colar17 from './colares-17.jpeg';
+import colar18 from './colares-18.jpeg';
+import colar19 from './colares-19.jpeg';
+import colar20 from './colares-20.jpeg';
+import colar21 from './colares-21.jpeg';
+import colar22 from './colares-22.jpeg';
+import colar23 from './colares-23.jpeg';
+import colar24 from './colares-24.jpeg';
+import colar25 from './colares-25.jpeg';
+
 
 // Exporta um array com todas as imagens, usando os novos nomes de variáveis
 export const COLARES = [
-  colar1, colar2, colar3, colar4, colar5, colar6
+  colar1, colar2, colar3, colar4, colar5, colar6, colar7, colar8, colar9, colar10,
+  colar11, colar12, colar13, colar14, colar15, colar16, colar17, colar18, colar19, colar20,
+  colar21, colar22, colar23, colar24, colar25
 ];
