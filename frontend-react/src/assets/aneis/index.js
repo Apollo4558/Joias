8c2a14cb3519b2f25c64abc0c8fdc081749911e6
rianfernandesno <zrpapz12@gmail.com>
2025-09-06 10:11:33 -0300

    aneis e conjuntos

---
+++ frontend-react/src/assets/aneis/index.js
@@ -0,0 +1,35 @@
+// src/assets/pingentes/index.js
+
+// Importa as imagens com os novos nomes de variáveis e arquivos
+import anel1 from './aneis-1.jpeg';
+import anel2 from './aneis-2.jpeg';
+import anel3 from './aneis-3.jpeg';
+import anel4 from './aneis-4.jpeg';
+import anel5 from './aneis-5.jpeg';
+import anel6 from './aneis-6.jpeg';
+import anel7 from './aneis-7.jpeg';
+import anel8 from './aneis-8.jpeg';
+import anel9 from './aneis-9.jpeg';
+import anel10 from './aneis-10.jpeg';
+import anel11 from './aneis-11.jpeg';
+import anel12 from './aneis-12.jpeg';
+import anel13 from './aneis-13.jpeg';
+import anel14 from './aneis-14.jpeg';
+import anel15 from './aneis-15.jpeg';
+import anel16 from './aneis-16.jpeg';
+import anel17 from './aneis-17.jpeg';
+import anel18 from './aneis-18.jpeg';
+import anel19 from './aneis-19.jpeg';
+import anel20 from './aneis-20.jpeg';
+import anel21 from './aneis-21.jpeg';
+import anel22 from './aneis-22.jpeg';
+import anel23 from './aneis-23.jpeg';
+import anel24 from './aneis-24.jpeg';
+import anel25 from './aneis-25.jpeg';
+
+// Exporta um array com todas as imagens, usando os novos nomes de variáveis
+export const ANEIS_IMAGES = [
+  anel1, anel2, anel3, anel4, anel5, anel6, anel7, anel8, anel9, anel10, anel11, anel12,
+  anel13, anel14, anel15, anel16, anel17, anel18, anel19, anel20, anel21, anel22, anel23,
+  anel24, anel25,
+];
