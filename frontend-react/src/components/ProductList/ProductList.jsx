5624e2f28933a9e4557a1bd3d6c3c749be07f897
rianfernandesno <zrpapz12@gmail.com>
2025-09-19 20:16:33 -0300

    alteracao nome

--- frontend-react/src/components/ProductList/ProductList.jsx
+++ frontend-react/src/components/ProductList/ProductList.jsx
@@ -5,8 +5,7 @@ import './ProductList.css';
 const ProductList = ({ products }) => {
   const handleAddToCart = (product) => {
     // Substitua SEU_NUMERO_DE_WHATSAPP pelo número de telefone da sua loja (incluindo o código do país)
-    const whatsappNumber = '77998161239'; 
-
+    const whatsappNumber = '7798476560'; 
     const message = encodeURIComponent(
       `Olá, gostaria de comprar o seguinte produto: \n` +
       `Produto: ${product.name}\n` +
